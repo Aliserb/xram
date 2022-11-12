8ab1d68a82cb661bc0f41c07921af285a50d62b1
veroni1337 <aliserb773@gmail.com>
2022-11-12 20:18:37 +0200

    inner news

--- src/assets/js/carousel.js
+++ src/assets/js/carousel.js
@@ -1,4 +1,4 @@
-var swiper = new Swiper(".calendar_swiper", {
+var calendar = new Swiper(".calendar_swiper", {
     slidesPerView: 3,
     spaceBetween: 30,
     navigation: {
@@ -7,11 +7,39 @@ var swiper = new Swiper(".calendar_swiper", {
     },
 });
 
-var swiper = new Swiper(".ask_question_swiper", {
+var askQuestion = new Swiper(".ask_question_swiper", {
     slidesPerView: 1,
     spaceBetween: 30,
     allowTouchMove: false,
     navigation: {
         nextEl: ".ask_question_next button",
+    },
+});
+
+var newsGallery = new Swiper(".news_gallery_thumblains", {
+    spaceBetween: 37,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesProgress: true,
+});
+
+var newsGalleryThumbs = new Swiper(".news_gallery", {
+    loop: true,
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".news_gallery_next",
+        prevEl: ".news_gallery_prev",
+    },
+    thumbs: {
+        swiper: newsGallery,
+    },
+});
+
+var moreNews = new Swiper(".more_news", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    navigation: {
+        nextEl: ".more_news_next",
+        prevEl: ".more_news_prev",
     },
 });
